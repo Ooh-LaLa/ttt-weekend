f5cec248b5c269907c9b99f1ed48c48222b48336
ooh-lala <laurentozzi.code@gmail.com>
2022-12-11 21:33:47 -0500

    complete ttt

--- js/app.js
+++ js/app.js
@@ -1,54 +1,138 @@
 /*-------------------------------- Constants --------------------------------*/
 const choices = ['X', 'O']
 
+winningCombos = [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner, tie
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
-// console.log(sqr);
-const messageEl = document.querySelector('.message')
-// console.log(message);
+
+const messageEl = document.querySelector('#message')
+console.log(messageEl);
+
+const boardEl = document.querySelector('.board')
+console.log(boardEl);
+
+const resetBtnEl = document.querySelector('.resetBtn')
+
 /*----------------------------- Event Listeners -----------------------------*/
 
+resetBtnEl.addEventListener("click", init)
+console.log(resetBtnEl);
 
-// document.getElementById("rock").addEventListener("click", play)
-// document.getElementById("paper").addEventListener("click", play)
-// document.getElementById("scissors").addEventListener("click", play)
+boardEl.addEventListener("click", handleClick)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-// function init() 
 
-// render()
 
- board = [null, null, null, null, null, null, null, null, null]
+function init() {
+    board = [null, null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = false
+    tie = false
+    render()
+}
 
- turn = 1
 
- winner = false
+function render(){
+    updateBoard()
+    updateMessage()
+}
 
- tie = false
+init()
 
 function updateBoard() {
-    for (i = 0; i < board.length; i++) {
-        let currentSqr = squareEls[i]
-        console.log(currentSqr);
-        currentSqr.textContent = ""
-       if (board[i] === -1){
-            currentSqr.textContent = "O" 
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === -1) {
+            squareEls[i].innerHTML = "O" 
+            console.log(squareEls[i]);
         } else if (board[i] === 1) {
-            currentSqr.textContent = "X"
+            squareEls[i].innerHTML = "X"
+        } else {    
+        console.log(board);
+            squareEls[i].innerHTML = ""
         }
     }     
 }
 
-updateBoard()
+
+function updateMessage() {
+  if (winner === false && tie === false)  {
+    messageEl.innerHTML = "Player turn"
+} else if (winner === false && tie === true) {
+    messageEl.innerHTML = "It's a tie"
+} else {
+    messageEl.innerHTML = "Congrats!"
+}
+} 
 
 
-// function updateMesaqge() {
-//     return `${winner} && ${tie} === false ? ${turn} :
-      
-// }
+function handleClick(evt) {
+    const sqIdx = Number(evt.target.id.slice(2))
+    console.log(sqIdx);
+if (board[sqIdx] !== null) {
+    return 
+} else if (winner === true) {
+    return
+}
+    placePiece(sqIdx)
+    checkForTie()
+    checkForWinner()
+    switchPlayerTurn()
+    render()
+}
+
+
+function placePiece(idx) {
+    board[idx] = turn
+}
+
+
+function checkForTie() {
+    if (!board.includes(null)) {
+    return tie = true
+} 
+}
+
+
+function checkForWinner() {
+          if(board[0] + board[1] + board[2] === 3 || board[0] + board[1] + board[2] === -3) {
+            winner = true
+          } 
+          if (board[3] + board[4] + board[5] === 3 || board[0] + board[1] + board[2] === -3) {
+            winner = true
+          }
+          if(board[6] + board[7] + board[8] === 3 || board[6] + board[7] + board[8] === -3) {
+            winner = true
+          }
+          if(board[0] + board[3] + board[6] === 3 || board[0] + board[3] + board[6] === -3) {
+            winner = true
+          }
+          if(board[1] + board[4] + board[7] === 3 || board[1] + board[4] + board[7] === -3) {
+            winner = true
+          }
+          if(board[2] + board[5] + board[8] === 3 || board[2] + board[5] + board[8] === -3) {
+            winner = true
+          }
+          if(board[0] + board[4] + board[8] === 3 || board[0] + board[4] + board[8] === -3) {
+            winner = true
+          }   
+            if(board[6] + board[4] + board[2] === 3 || board[6] + board[4] + board[2] === -3) {
+            winner = true
+          }
+    }
+         
+
+    function switchPlayerTurn() {
+        if(winner === true){
+        return
+    }
+        else {
+            turn *= -1
+        }
+    }
+
